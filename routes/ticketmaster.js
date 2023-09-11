9ea95bc62192105e671ed3155a39a1859022c369
virus2207 <100256996+virus2207@users.noreply.github.com>
2023-09-11 15:54:39 +1000

    ticketmaster response with images

--- routes/ticketmaster.js
+++ routes/ticketmaster.js
@@ -14,38 +14,34 @@ router.get('/events', async function (req, res) {
         const ticketmaster_url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketmaster_api}&city=${city}`
 
         const ticketMasterResponse = await axios.get(ticketmaster_url);
-        const eventData = ticketMasterResponse.data._embedded.events[0];
+        const eventData = ticketMasterResponse.data._embedded.events;
         //extract data 
+        const eventDetailsList = await Promise.all(eventData.map(async (event) => {
+            const address = `${event._embedded.venues[0].address.line1},${event._embedded.venues[0].city.name}`;
+            images = event.images[2].url;
+            const eventDetails = {
+                name: event.name,
+                id: event.id,
+                address: address,
+                link: event.url,
+                image: images
+            }
 
-        const eventDetails = {
-            name: eventData.name,
-            id: eventData.id,
-            address: `${eventData._embedded.venues[0].address.line1},${eventData._embedded.venues[0].city.name}`,
-            link: eventData.url,
-        }
+            const googleMapsUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${eventDetails.address}&key=${googleApiKey}`;
+            const geoResponse = await axios.get(googleMapsUrl);
+            const geoLocation = geoResponse.data.results[0];
+            const formattedAddress = geoLocation.formatted_address;
+            const latitude = geoLocation.geometry.location.lat;
+            const longitude = geoLocation.geometry.location.lng;
 
-        // use the address from eventdetails to fectch geocode
+            eventDetails.formatted_address = formattedAddress;
+            eventDetails.geometry = `${latitude},${longitude}`;
 
-        const googleMapsUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(eventDetails.address)}&key=${googleApiKey}`;
-        const geoResponse = await axios.get(googleMapsUrl);
-        const geoLocation = geoResponse.data.results[0];
-        const formattedAddress = geoLocation.formatted_address;
-        const latitude = geoLocation.geometry.location.lat;
-        const longitude = geoLocation.geometry.location.lng;
+            return eventDetails;
+        }));
+        res.json(eventDetailsList)
 
-        eventDetails.formatted_address = formattedAddress;
-        eventDetails.geometry = `${latitude},${longitude}`;
 
-        const htmlResponse = `
-              <p><strong>Event Name:</strong> ${eventDetails.name}</p>
-              <p><strong>Event ID:</strong> ${eventDetails.id}</p>
-              <p><strong>Address:</strong> ${eventDetails.formatted_address}</p>
-              <p><strong>Link:</strong> <a href="${eventDetails.link}">${eventDetails.link}</a></p>
-              <p><strong>Geolocation:</strong> ${eventDetails.geometry}</p>
-               `;
-
-        res.send(htmlResponse);
-        //return res.json(response.data); // return the whole data 
     } catch (error) {
         console.error(error);
         res.status(500).send("An error occur");
