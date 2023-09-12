230c2d2f80368849f60b430d029b35b40526b78d
virus2207 <100256996+virus2207@users.noreply.github.com>
2023-09-13 01:21:19 +1000

    modified route

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const externalRoute = require('./routes/route')
 const ticketmaster = require('./routes/ticketmaster')
+const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = 3000;
+app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => {
     res.send("Hello Express!")
 });
 
-app.use('/ticketmaster', ticketmaster);
+app.use('/events', ticketmaster);
 
 
 app.use('/search', externalRoute);
--- routes/ticketmaster.js
+++ routes/ticketmaster.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const ticketmaster_api = process.env.TICKET_MASTER_API;
 const googleApiKey = process.env.GOOGLE_API;
 
-router.get('/events', async function (req, res) {
+router.get('/', async function (req, res) {
     try {
 
         //const size = 10;
@@ -24,7 +24,10 @@ router.get('/events', async function (req, res) {
                 id: event.id,
                 address: address,
                 link: event.url,
-                image: images
+                image: images,
+                date: event.dates.start.localDate,
+                time: event.dates.start.localTime,
+                venue: event._embedded.venues[0].name
             }
 
             const googleMapsUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${eventDetails.address}&key=${googleApiKey}`;
@@ -34,12 +37,27 @@ router.get('/events', async function (req, res) {
             const latitude = geoLocation.geometry.location.lat;
             const longitude = geoLocation.geometry.location.lng;
 
+
             eventDetails.formatted_address = formattedAddress;
             eventDetails.geometry = `${latitude},${longitude}`;
+            //eventDetails.googleMapLink = `https://maps.googleapis.com/maps/api/directions/json?origin=Brisbane&destination=${eventDetails.geometry}&key=${process.env.GOOGLE_API}`
+
 
             return eventDetails;
+
+            //         const htmlResponse = `
+            //     <p><strong>Event Name:</strong> ${eventDetails.name}</p>
+            //     <p><strong>Event ID:</strong> ${eventDetails.id}</p>
+            //     <p><strong>Address:</strong> ${eventDetails.formatted_address}</p>
+            //     <!--<p><strong>Google Maps Link:</strong> <a href="https://www.google.com/maps?q=${latitude},${longitude}" target="_blank">Open in Google Maps</a></p>-->
+            //     <p><strong>Event Image:</strong> <img src="${eventDetails.image}" alt="${eventDetails.name}" width="200"></p>
+            //     <p><strong>Event Ticketmaster Link:</strong> <a href="${eventDetails.link}" target="_blank">Buy Tickets</a></p>
+            // `;
+
+            //         return htmlResponse;
         }));
-        res.json(eventDetailsList)
+        res.json(eventDetailsList);
+
 
 
     } catch (error) {
@@ -47,6 +65,27 @@ router.get('/events', async function (req, res) {
         res.status(500).send("An error occur");
     }
 })
+
+router.get('/direction/:start/:geometry', async function (req, res) {
+    try {
+
+        const geometry = req.params.geometry;
+        const start = req.params.start;
+        if (!geometry || !start) {
+            return res.status(400).send("Latitude, longitude, and startpoint parameters are required")
+
+        }
+
+        // Use lat, long, and startpoint from the query parameters to construct the Google Maps URL
+        const googleMapDirection = await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${start}&destination=${geometry}&key=${process.env.GOOGLE_API}`);
+
+        // Handle the googleMapDirection response here
+        res.json(googleMapDirection.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occur");
+    }
+});
 
 module.exports = router;
 
